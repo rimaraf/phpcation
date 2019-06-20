9bc3096c9c7100688fe7e869bb3a047207a17104
rimaraf <lassehcz@icloud.com>
2019-06-20 19:47:31 +0200

    Update config.php

--- config.php
+++ config.php
@@ -7,13 +7,8 @@
 // your MySQL database:
 // --------------------------------------------------------
 
-//$db_host = "host address for the MySQL database, e.g. 'localhost:8889' ";
-//$db_user = "username for the database, e.g. 'root' ";
-//$db_pass = "the database user password, e.g. 'root' ";
-//$db_name = "the name of the database, e.g. 'hobby_community' ";
-
-$db_host = "localhost:8889";
-$db_user = "root";
-$db_pass = "root";
-$db_name = "co2map";
+$db_host = "host address for the MySQL database, e.g. 'localhost:8889' ";
+$db_user = "username for the database, e.g. 'root' ";
+$db_pass = "the database user password, e.g. 'root' ";
+$db_name = "the name of the database, e.g. 'hobby_community' ";
 ?>
